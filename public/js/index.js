ba97b519400b4f31ee93e3a1c172d389f067db89
330namajnoon <sina.majnoonhjk@gmail.com>
2022-11-08 17:54:49 +0100

    1

--- public/js/index.js
+++ public/js/index.js
@@ -7,9 +7,36 @@ let rabetekarbari;
 function RabeteKarbari() {
     this.body = document.querySelector("body");
     this.paszamine = CrateElement({name:"div",class:"paszamine"});
-    this.file = CrateElement({name:"input",type:"file"});
     this.imgDiv = CrateElement({name:"div",class:"imgDiv"});
+    this.file = CrateElement({name:"input",type:"file"});
     this.img = CrateElement({name:"img"})
+
+    this.nameDiv = CrateElement({name:"div",class:"nameDiv"});
+    this.nameInput = CrateElement({name:"input",atriviuts:[{name:"placeholder",value:"Nombre"}]});
+    this.nameLine = CrateElement({name:"div"});
+
+    this.button = CrateElement({name:"input",type:"button",value:"ENTRAR"});
+
+    this.Crate();
+    this.imgDiv.addEventListener("click",()=> {
+        this.file.click();
+    })
+    this.img.addEventListener("change",()=> {
+        this.uploadImage();
+    })
+}
+RabeteKarbari.prototype.Crate = function() {
+    this.paszamine.appendChild(this.imgDiv);
+    this.paszamine.appendChild(this.file);
+    this.paszamine.appendChild(this.img);
+    this.nameDiv.appendChild(this.nameInput);
+    this.nameDiv.appendChild(this.nameLine);
+    this.paszamine.appendChild(this.nameDiv);
+    this.paszamine.appendChild(this.button);
+    this.body.appendChild(this.paszamine);
+}
+RabeteKarbari.prototype.uploadImage = function() {
+    let 
 }
 
 rabetekarbari = new RabeteKarbari();
